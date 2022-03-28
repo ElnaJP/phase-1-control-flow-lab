f6f0758b6417c1360dbf503980c89d206fc1dc69
Elna JP <eriosdd@gmail.com>
2022-03-27 23:55:19 -0400

    Assignment not fully completed

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride)
+{
+  if(ride <='400 feet') 
+  {
+  return 'This one is on me!'
+  }
+  else if('charges 30 dollars for a distance over 2000 feet') 
+  {
+   return 'I will gladly take your thirty bucks.'
+ } 
 }
+
+  
+
+  
+   
+      
+  
+  
+
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
